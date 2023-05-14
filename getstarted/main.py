0960010a334cd5ccb8d97a5cedd6e248137cd89f
Arash Khajooei Nejad <A.KhajooeiNejad@sic.net>
2023-05-14 14:41:51 +0330

    Initial commit

---
+++ getstarted/main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class Item :
+
+    def __init__(self,name,price,quantity,):
+        self.price=price
+        self.quantity=quantity
+        self.name=name
+
+    def calculate_the_result(self,x,y):
+        return x*y
+
+
+
+
+item1 = Item("phone",2,100)
+item2 = Item("new item",2,100)
+
+print(item2.name)
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
